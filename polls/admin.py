db0a1e14c8f47129e8b001179bee08350c6d37bf
wayne <wayneborong@gmail.com>
2019-09-12 16:30:23 +1000

    add url manipulation

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+#after you may check in the admin page
+from .models import Question
+from .models import Choice
+
+admin.site.register(Choice)
+admin.site.register(Question)
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+
+
 from django.http import HttpResponse
 # Create your views here.
 def index (request):
   return HttpResponse('hello,world inside a polls index')
+
+def detail(request,question_id):
+  return HttpResponse("you`re looking at question %s." %question_id)
+
+def results(request,question_id):
+  response ="you`re looking at the result of question  %s."
+  return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+  return HttpResponse('you`re voting on question %s.' %question_id)
